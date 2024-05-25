4a278dbfd14a79a839704b51707c3f9721b181a5
Ksenia Durkina <rustambyr69@gmail.com>
2024-05-25 14:32:42 +0300

    Update index.js

--- Back/index.js
+++ Back/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("Все гуд");
+  res.json("Все гуд");
+  console.log(res);
 });
 
 app.listen(3000, console.log("Good"));
