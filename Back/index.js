edeeecc1edee95c2defd11690dff1a0ba7c9230f
Ksenia Durkina <rustambyr69@gmail.com>
2024-05-25 14:29:55 +0300

    Send text

--- Back/index.js
+++ Back/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send(db.json);
+  res.send("Все гуд");
 });
 
 app.listen(3000, console.log("Good"));
