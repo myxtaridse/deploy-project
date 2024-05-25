bdd10ee15a618298b299de58aac2838bf48b61cb
Ksenia Durkina <rustambyr69@gmail.com>
2024-05-25 14:22:18 +0300

    Update back response

--- Back/index.js
+++ Back/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.send("Server is running");
+  res.send(db.json);
 });
 
 app.listen(3000, console.log("Good"));
