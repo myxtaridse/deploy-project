c0e6a63c121f8b583590a992ac5d4b243931a416
myxtaridse <134769935+myxtaridse@users.noreply.github.com>
2024-05-25 14:41:42 +0300

    Update index.js

--- Back/index.js
+++ Back/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const { json } = require("./db");
 const app = express();
 
 app.use("/", (req, res) => {
-  res.json("Все гуд");
-  console.log(res);
+  res.json(json);
 });
 
 app.listen(3000, console.log("Good"));
